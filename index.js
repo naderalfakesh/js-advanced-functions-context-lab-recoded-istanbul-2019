771b4e58da7cf57115a83d6c800da1f5ffb6444e
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-20 13:18:01 +0000

    Done.

--- index.js
+++ index.js
@@ -18,5 +18,57 @@ let allWagesFor = function () {
         return memo + wagesEarnedOnDate.call(this, d)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
+    return payable 
+}
+
+function createEmployeeRecord(arr){
+  return{
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  }
+}
+function createEmployeeRecords(arr){
+  return arr.map(elm => createEmployeeRecord(elm) )
+}
+
+function createEmployees(arr){
+  return arr.map(elm => createEmployeeRecord(elm) )
+}
+
+function createTimeInEvent(str){
+  const type = "TimeIn"
+  const hour = parseInt(str.split(" ")[1])
+  const date = str.split(" ")[0]
+  
+  this.timeInEvents.push({
+    type,
+    hour,
+    date
+  })
+  return this;
+}
+
+function createTimeOutEvent(str){
+  const type = "TimeOut"
+  const hour = parseInt(str.split(" ")[1])
+  const date = str.split(" ")[0]
+  
+  this.timeOutEvents.push({
+    type,
+    hour,
+    date
+  })
+  return this;
+}
+
+function hoursWorkedOnDate(str){
+ this.timeOutEvents.find(event =>)
+}
+
+function hoursWorkedOnDate(str){
+ 
 }
